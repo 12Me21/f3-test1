223fd02322bd0673c4febf5191afe10795c0bda2
12Me21 <12Me21.mc@gmail.com>
2025-08-26 10:35:48 -0700

    start

---
+++ Makefile
@@ -0,0 +1,11 @@
+AS = asl
+ASFLAGS = -i . -i .. -n -U -q +t 70 -r 1
+P2BIN = p2bin
+
+all: test.bin
+
+%.o: %.s
+	$(AS) $< $(ASFLAGS) -o $@
+
+%.bin: %.o
+	$(P2BIN) $< $@ -r 0x0-0x1FFFFF
---
+++ run.sh
@@ -0,0 +1,2 @@
+cp test.bin ~/Code/mame2/roms/all/twelve-test1.bin
+~/Code/mame2/taito_f3 -nokeepaspect -nofilter -nounevenstretch -window -nomaximize -intscalex 2 -intscaley 2 -skip_gameinfo twelvetest1
