9249c46c5be29f75d6beebc62abc3020d27c9d6c
12Me21 <12Me21.mc@gmail.com>
2025-09-09 14:02:26 -0700

    trying audiocpu

---
+++ audiocpu.s
@@ -0,0 +1,40 @@
+		CPU 68000
+		SUPMODE ON
+		PADDING ON
+		ORG $C00000
+	
+ROM_VECTORS_0:
+	dc.l	$00000000
+	dc.l	entry
+	dc.l  [75]exc
+	
+spin:	
+	bra spin
+
+exc:	
+	rte
+	
+entry:	
+	moveq.l #$7F, D0
+	move.l D0, D1
+	neg.l D1
+.wait:
+	suba.l A1, A1
+	move.l D0, (A1)+
+	move.l D1, (A1)
+	cmp.l (A1), D1
+	bne .check1_fail
+	cmp.l -(A1), D0
+.check1_fail:
+	bne .wait
+	
+	movea.l (ROM_VECTORS_0), SP
+	
+	moveq.l #76, D1
+	lea ROM_VECTORS_0, A1
+	lea 0, A0
+.write_vectors:
+	move.l (A1)+, (A0)+
+	dbf D1, .write_vectors
+	
+	jmp spin
